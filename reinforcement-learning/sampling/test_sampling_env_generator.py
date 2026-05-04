5d09b0654f2a994c386a24408d216b704198b53a
Manuel Meyer <maenu.meyer@bluewin.ch>
2026-05-04 15:29:36 +0200

    feat: add test for line length = 3

--- reinforcement-learning/sampling/test_sampling_env_generator.py
+++ reinforcement-learning/sampling/test_sampling_env_generator.py
@@ -22,3 +22,21 @@ def test_sampling_env_generator() -> None:
     env.reset()
     assert np.array_equal(env.rail.grid, env_raw.rail.grid)
     assert env.agents == env_raw.agents
+
+
+def test_sampling_env_generator_line_length_3() -> None:
+    sampling_env = sampling_env_generator(RailEnvPersister.load_new("level_0_scenario_1.pkl", load_from_package="competitions.competition_2026.level_0")[0], line_length=3)
+    env, _ = RailEnvPersister.load_new("level_0_scenario_1.pkl", load_from_package="competitions.competition_2026.level_0")
+
+    assert np.array_equal(env.rail.grid, sampling_env.rail.grid)
+    assert env.agents == sampling_env.agents
+
+    # verify sampling env produces new randomly sampled lines on the same grid
+    sampling_env.reset()
+    assert np.array_equal(env.rail.grid, sampling_env.rail.grid)
+    assert len(env.agents) == len(sampling_env.agents)
+    assert env.agents != sampling_env.agents
+
+    # check if maximum line length is at most 3
+    for agent in sampling_env.agents:
+        assert len(agent.waypoints) <= 3, f"Agent {agent.handle} has waypoints {agent.waypoints} with length {len(agent.waypoints)} > 3"
